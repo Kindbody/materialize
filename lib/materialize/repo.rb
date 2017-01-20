7c65cfc3f5ceff468beaa20e9bd275502cf21efc
Stephen Fiser <stephen@bluebear.io>
2017-01-19 22:56:03 -0500

    working on the repo

--- lib/materialize/repo.rb
+++ lib/materialize/repo.rb
@@ -2,8 +2,47 @@ module Materialize
   class Repo
 
     def method_missing(query, *args, &block)
-      data_source = args[0]
-      data_source.send(query)
+      data_source_class = args[0]
+      options           = args[1] || {}
+      args_to_pass      = options[:args]
+
+      data, entity_class = process(data_source_class, query, args_to_pass)
+
+      if data.is_a?(Array)
+        entity_class.wrap(data)
+      else
+        entity_class.new(data)
+      end
+    end
+
+    private
+
+    def process(data_source_class, query, args_to_pass)
+      data = get_data(data_source_class, query, args_to_pass)
+      entity_class = entity_class_for entity_class_name_for base_class_name_for data_source_class
+      return data, entity_class
+    end
+
+    def entity_class_for(entity_class_name)
+      entity_class_name.split('::').reduce(Module, :const_get)
+    end
+
+    def entity_class_name_for(base_class_name)
+      "Entities::#{base_class_name}"
+    end
+
+    def base_class_name_for(data_source_class)
+      data_source_class.name.split('::').last
+    end
+
+    def get_data(data_source_class, query, args_to_pass)
+      if args_to_pass.nil?
+        data_source_class.send(query)
+      elsif args_to_pass.is_a?(Array)
+        data_source_class.send(query, *args_to_pass)
+      else
+        data_source_class.send(query, args_to_pass)
+      end
     end
 
   end
--- spec/repo_spec.rb
+++ spec/repo_spec.rb
@@ -2,13 +2,67 @@ require 'spec_helper'
 
 describe Materialize::Repo do
 
-  it 'passes the query along to the data source' do
-    repo = Materialize::Repo.new
-    data_source = double('data_source')
-    bob = { id: 1, name: 'Bob the zombie' }
-    expect(data_source).to receive(:find_all_zombies).and_return([bob])
+  let(:repo) { Materialize::Repo.new }
 
-    repo.find_all_zombies(data_source)
+  it 'returns a collection of entities containing the data from the data source if the result is an []' do
+    zombies = repo.find_all_zombies(DataSource::Zombie)
+    zombie  = zombies.first
+    expect(zombie.class).to eq(Entities::Zombie)
+    expect(zombie.name).to eq('Bob the zombie')
+    expect(zombie.id).to eq(1)
+    expect(zombies.count).to eq(3)
   end
 
+
+  it 'also handles the case where the argument is an array' do
+    zombies = repo.find_two_zombies(DataSource::Zombie, args: [1,2])
+    zombie  = zombies.last
+    expect(zombie.name).to eq('Sarah the zombie')
+    expect(zombie.id).to eq(2)
+    expect(zombies.count).to eq(2)
+  end
+
+  it 'returns an individual entity containing the data from the data source if the result is a {}' do
+    zombie = repo.find_one_zombie(DataSource::Zombie, args: 2)
+    expect(zombie.class).to eq(Entities::Zombie)
+    expect(zombie.name).to eq('Sarah the zombie')
+    expect(zombie.id).to eq(2)
+  end
+
+end
+
+module DataSource
+  class Zombie
+    class << self
+
+      def find_all_zombies
+        [{ id: 1, name: 'Bob the zombie' }, { id: 2, name: 'Sarah the zombie' }, { id: 3, name: 'Me the zombie' }]
+      end
+
+      def find_two_zombies(id_1, id_2)
+        [{ id: 1, name: 'Bob the zombie' }, { id: 2, name: 'Sarah the zombie' }]
+      end
+
+      def find_one_zombie(id)
+        { id: 2, name: 'Sarah the zombie' }
+      end
+    end
+  end
+end
+
+module Entities
+  class Zombie
+
+    def self.wrap(zombies_data)
+      zombies_data.map { |zombie_data| new(zombie_data)  }
+    end
+
+    attr_reader :id, :name
+
+    def initialize(attributes)
+      @id = attributes[:id]
+      @name = attributes[:name]
+    end
+
+  end
 end
