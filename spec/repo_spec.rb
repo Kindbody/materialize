492a8eb8c81aa35934c2a98ac4386020fa6189e4
Stephen Fiser <stephen@bluebear.io>
2017-01-19 21:50:51 -0500

    removes weird gemspec error

--- spec/repo_spec.rb
+++ spec/repo_spec.rb
@@ -5,7 +5,8 @@ describe Materialize::Repo do
   it 'passes the query along to the data source' do
     repo = Materialize::Repo.new
     data_source = double('data_source')
-    expect(data_source).to receive(:find_all_zombies)
+    bob = { id: 1, name: 'Bob the zombie' }
+    expect(data_source).to receive(:find_all_zombies).and_return([bob])
 
     repo.find_all_zombies(data_source)
   end
