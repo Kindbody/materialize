82a4d9be18536b2c0d61a69b42394857123d5264
Stephen Fiser <stephen@bluebear.io>
2017-01-22 22:26:02 -0500

    scope fix

--- lib/materialize/concurrent.rb
+++ lib/materialize/concurrent.rb
@@ -1,13 +1,15 @@
-module Concurrent
+module Materialize
+  module Concurrent
 
-  def concurrent(*lambdas)
-    threads = []
-    lambdas.each do |l|
-      threads << Thread.new do
-        l.()
+    def concurrent(*lambdas)
+      threads = []
+      lambdas.each do |l|
+        threads << Thread.new do
+          l.()
+        end
       end
+      threads.each(&:join)
     end
-    threads.each(&:join)
-  end
 
+  end
 end
--- lib/materialize/version.rb
+++ lib/materialize/version.rb
@@ -1,3 +1,3 @@
 module Materialize
-  VERSION = "0.2.3"
+  VERSION = "0.2.4"
 end
