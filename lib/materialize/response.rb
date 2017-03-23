8d8ad1f7981c9665301782d85d154c367bfef705
Stephen Fiser <stephen@bluebear.io>
2017-03-23 19:33:03 -0400

    nests response inside of materialize module

--- lib/materialize/response.rb
+++ lib/materialize/response.rb
@@ -1,20 +1,22 @@
-class Response
+module Materialize
+  class Response
 
-  attr_reader :message, :status, :options
+    attr_reader :message, :status, :options
 
-  def initialize(options)
-    @message = options['message']
-    @status  = options['status'].to_sym
-    @options = options
+    def initialize(options)
+      @message = options['message']
+      @status  = options['status'].to_sym
+      @options = options
+    end
+
+    def ok?
+      status == :ok
+    end
+
+    # This is used to look up ids -> e.g. patient_id, phase_id, etc.
+    def method_missing(message)
+      options.send(:[], message.to_s)
+    end
+
   end
-
-  def ok?
-    status == :ok
-  end
-
-  # This is used to look up ids -> e.g. patient_id, phase_id, etc.
-  def method_missing(message)
-    options.send(:[], message.to_s)
-  end
-
 end
